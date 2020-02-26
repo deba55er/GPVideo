080758e61d722a4cf3b600dbe89c17d0fb88a730
Andrei Koltšenko <andrei.koltsenko@ttu.ee>
2020-02-26 23:44:26 +0200

    correcting errors in names

--- Soft/Areas/Quantity/Pages/Measures/Index.cshtml.cs
+++ Soft/Areas/Quantity/Pages/Measures/Index.cshtml.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using Abc.Facade;
 using Abc.Facade.Quantity;
 using Abc.Pages.Quantity;
-using Abc.Soft.Data;
 
 namespace Abc.Soft
 {
