302a3c2aea7c39e99d77552edb51f7171d3fba80
Andrei Koltšenko <andrei.koltsenko@ttu.ee>
2020-02-27 09:53:09 +0200

    Added DisplayControlForHtmlExtension, updated Create, Edit pages

---
+++ Pages/Extensions/DisplayControlsForHtmlExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Html;
+
+namespace Abc.Pages.Extensions
+{
+    public static class DisplayControlsForHtmlExtension
+    {
+        public static IHtmlContent DisplayControlsFor<TClassType, TPropertyType>(
+            this IHtmlHelper<TClassType> htmlHelper, Expression<Func<TClassType, TPropertyType>> expression)
+        {
+            var s = new List<object>
+            {
+                new HtmlString("<dt class=\"col-sm-2\">"),
+                htmlHelper.DisplayNameFor(expression),
+                new HtmlString("</dt>"),
+                new HtmlString("<dd class=\"col-sm-10\">"),
+                htmlHelper.DisplayFor(expression),
+                new HtmlString("</dd>"),
+            };
+
+            return new HtmlContentBuilder(s);
+        }
+    }
+}
