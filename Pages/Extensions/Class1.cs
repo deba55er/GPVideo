75dcef711d6cd21b52f8f33c9968d0c2ed88de83
Andrei Koltšenko <andrei.koltsenko@ttu.ee>
2020-02-27 09:18:16 +0200

    Added Extension EditControlsForHtmlExtension

---
+++ Pages/Extensions/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Abc.Pages.Extensions
+{
+    public static class EditControlsForHtmlExtension
+    {
+
+
+        public static IHtmlContent EditControlsFor<TClassType, TPropertyType>(
+            this IHtmlHelper<TClassType> htmlHelper, Expression<Func<TClassType, TPropertyType>> expression)
+        {
+
+            //<div class="form-group">
+            //    <label asp-for="Item.Code" class="control-label"></label>
+            //    <input asp-for="Item.Code" class="form-control" />
+            //    <span asp-validation-for="Item.Code" class="text-danger"></span>
+            //</div>
+
+            var htmlStrings = new List<object>
+            {
+                new HtmlString("<div class=\"form-group\">"),
+                htmlHelper.LabelFor(expression, new {@class = "text-dark"}),
+                htmlHelper.EditorFor(expression,
+                    new {htmlAttributes = new {@class = "form-control"}}),
+                htmlHelper.ValidationMessageFor(expression, "", new {@class = "text-danger"}),
+                new HtmlString("</div>")
+            };
+
+            return new HtmlContentBuilder(htmlStrings);
+        }
+    }
+}
