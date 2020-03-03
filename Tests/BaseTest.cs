b1ab4be11b4d26ad503fa8e8c968b618514ca1d4
Andrei Koltšenko <andrei.koltsenko@ttu.ee>
2020-03-03 23:56:24 +0200

    Added 4 tests and timelog update

---
+++ Tests/BaseTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    public abstract class BaseTest<TClass, TBaseClass> where TClass : new()
+    {
+        [TestMethod]
+        public void CanCreateTest()
+        {
+            Assert.IsNotNull(new TClass());
+        }
+
+        [TestMethod]
+        public void InheritedTest()
+        {
+            Assert.AreEqual(typeof(TBaseClass),
+                new TClass().GetType().BaseType);
+        }
+    }
+}
---
+++ Tests/Data/Quantity/MeasureDataTests.cs
@@ -0,0 +1,12 @@
+using Abc.Data.Common;
+using Abc.Data.Quantity;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Data.Quantity
+{
+    [TestClass]
+    public class MeasureDataTests: BaseTest<MeasureData, DefinedEntityData>
+    {
+    }
+}
