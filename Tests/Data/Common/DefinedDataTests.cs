2203c00c31b616829b02170a8533e5264e68f0aa
Andrei Koltšenko <andrei.koltsenko@ttu.ee>
2020-03-05 10:27:00 +0200

    23 tests passed

--- Tests/Data/Common/DefinedDataTests.cs
+++ Tests/Data/Common/DefinedDataTests.cs
@@ -6,13 +6,24 @@ namespace Tests.Data.Common
     [TestClass]
     public class DefinedDataTests : AbstractClassTest<DefinedEntityData, NamedEntityData>
     {
-        private class testClass : DefinedEntityData { }
+        private class testClass : DefinedEntityData
+        {
+        }
 
         [TestInitialize]
         public override void TestInitialize()
         {
             base.TestInitialize();
             obj = new testClass();
+        }
+
+        [TestMethod]
+        public void DefinitionTest()
+        {
+            var s = "AAAAA";
+            Assert.AreNotEqual(s, obj.Definition);
+            obj.Definition = s;
+            Assert.AreEqual(s, obj.Definition);
         }
     }
 }
--- Tests/Data/Common/PeriodDataTests.cs
+++ Tests/Data/Common/PeriodDataTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Abc.Data.Common;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -15,6 +16,24 @@ namespace Tests.Data.Common
         {
             base.TestInitialize();
             obj = new testClass();
+        }
+
+        [TestMethod]
+        public void ValidFromTest()
+        {
+            var d = DateTime.Now;
+            Assert.AreNotEqual(d, obj.ValidFrom);
+            obj.ValidFrom = d;
+            Assert.AreEqual(d, obj.ValidFrom);
+        }
+
+        [TestMethod]
+        public void ValidToTest()
+        {
+            var d = DateTime.Now;
+            Assert.AreNotEqual(d, obj.ValidTo);
+            obj.ValidTo = d;
+            Assert.AreEqual(d, obj.ValidTo);
         }
     }
 }
