7dedb7885d172e014e95b679929fd0e0708c2b1e
Andrei Koltšenko <andrei.koltsenko@ttu.ee>
2020-04-04 23:10:44 +0300

    V48, 302 testi läbitud, Fasaadi testid tehtud.

---
+++ Tests/Facade/Quantity/CommonTermViewTests.cs
@@ -0,0 +1,28 @@
+﻿using Abc.Facade.Common;
+using Abc.Facade.Quantity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Abc.Tests.Facade.Quantity
+{
+    [TestClass]
+    public class CommonTermViewTests : AbstractClassTests<CommonTermView, PeriodView>
+    {
+        private class TestClass : CommonTermView
+        {
+        }
+
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            base.TestInitialize();
+            obj = new TestClass();
+        }
+
+        [TestMethod]
+        public void TermIdTest() => IsNullableProperty(() => obj.TermId, x => obj.TermId = x);
+
+        [TestMethod]
+        public void PowerTest() => IsProperty(() => obj.Power, x => obj.Power = x);
+
+    }
+}
