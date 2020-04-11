605edae46a0d88fbbca206a66532615024285205
Andrei Koltšenko <andrei.koltsenko@ttu.ee>
2020-04-11 22:15:31 +0300

    V57, Infra Quantity repo testid, UnitFactors, UnitTerms

--- Tests/Infra/Quantity/UnitTermsRepositoryTests.cs
+++ Tests/Infra/Quantity/UnitTermsRepositoryTests.cs
@@ -1,12 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Abc.Aids;
+using Abc.Data.Quantity;
+using Abc.Domain.Quantity;
+using Abc.Infra;
+using Abc.Infra.Quantity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Abc.Tests.Infra.Quantity
 {
     [TestClass]
-    public class UnitTermsRepositoryTests
+    public class UnitTermsRepositoryTests : RepositoryTests<UnitTermsRepository, UnitTerm, UnitTermData>
     {
+
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            var options = new DbContextOptionsBuilder<QuantityDbContext>()
+                .UseInMemoryDatabase("TestDb").Options;
+            db = new QuantityDbContext(options);
+            dbSet = ((QuantityDbContext)db).UnitTerms;
+            obj = new UnitTermsRepository((QuantityDbContext)db);
+            base.TestInitialize();
+        }
+
+        protected override Type GetBaseType() => typeof(PaginatedRepository<UnitTerm, UnitTermData>);
+
+        protected override string GetId(UnitTermData d) => $"{d.MasterId}.{d.TermId}";
+
+        protected override UnitTerm GetObject(UnitTermData d) => new UnitTerm(d);
+
+        protected override void SetId(UnitTermData d, string id)
+        {
+            var masterId = GetString.Head(id);
+            var termId = GetString.Tail(id);
+
+            d.MasterId = masterId;
+            d.TermId = termId;
+        }
+
     }
+
 }
